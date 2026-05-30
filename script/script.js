a350e389ab0798ead472646afdb1ddb84dbb97eb
james smart osaze <jamessmartosaze65@gmail.com>
2026-05-30 16:31:14 +0800

    rebuilding code

--- script/script.js
+++ script/script.js
@@ -8,4 +8,22 @@ chatBtn.addEventListener("click",function(){
         text.style.display = "block"
     }
 })
-     
+
+ const toggle = document.getElementById("toggle");
+toggle.addEventListener("click",function(){
+ document.body.classList.toggle("mode")
+   
+}
+)
+const p = document.getElementById("p");
+const hiddenBtn = document.getElementById("hiddenBtn");
+hiddenBtn.addEventListener("click", function(){
+    
+    if(p.style.display ==="block"){
+        p.style.display = 'none'
+    }else{
+        p.style.display = "block"
+    }
+}
+
+)
