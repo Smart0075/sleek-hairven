28c2299574fc26c2590e10e6f1f37101ed9c6a53
james smart osaze <jamessmartosaze65@gmail.com>
2026-06-03 17:26:56 +0800

    writting

--- script/script.js
+++ script/script.js
@@ -38,3 +38,18 @@ hiddenBtn.addEventListener("click", function(){
 }
 
 )
+
+
+const slides = document.querySelector(".slides");
+const images = document.querySelectorAll(".slides img");
+let index = 0
+setInterval(() => {
+    index++;
+    if (index >= images.length) {
+        index = 0;
+    }
+    slides.style.transform = `translateX(-${index *100}%)`;
+}, 3000);
+
+
+
