867f3429bbdb9591c73f7ca8a7e5425eee6e7854
james smart osaze <jamessmartosaze65@gmail.com>
2026-06-04 15:08:32 +0800

    fixing

--- script/script.js
+++ script/script.js
@@ -42,14 +42,52 @@ hiddenBtn.addEventListener("click", function(){
 
 const slides = document.querySelector(".slides");
 const images = document.querySelectorAll(".slides img");
+const dot = document.querySelectorAll(".dot");
+
 let index = 0
-setInterval(() => {
+
+function showSlide(){
+    slides.style.transform = `translate(-${index * 100}%)`;
+
+dot.forEach(dot => dot.classList. remove("active")); {
+  dot[index].classList.add("active")  
+}
+}
+
+function nextSlide(){
     index++;
-    if (index >= images.length) {
+    if(index >= images.length){
         index = 0;
     }
-    slides.style.transform = `translateX(-${index *100}%)`;
-}, 3000);
+    showSlide()
 
+}
 
+function prevSlide(){
+    index--;
 
+    if(index < 0){
+        index = images.length - 1
+    }
+    showSlide()
+}
+
+let autoSlide = setInterval(nextSlide,3000);
+document.getElementById("next").addEventListener("click", nextSlide);
+document.getElementById("prev").addEventListener("click",prevSlide);
+
+const slider = document.querySelector(".slider");
+slider.addEventListener("mouseenter",() => {
+    clearInterval(autoSlide);
+})
+
+slider.addEventListener("mouseleave", () =>{
+    autoSlide = setInterval(nextSlide, 3000);
+})
+
+dot.forEach(dot =>{
+    dot.addEventListener("click", () => {
+
+    } )
+})
+showSlide();
